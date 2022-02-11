4488e2c77099bc24afb4eb56debb5766c7d4a61b
Hoefkens Jeroen <hoefkens.j@gmail.com>
2022-02-11 10:02:55 +0100

    fixing ansi cli

--- fnx/portal.py
+++ fnx/portal.py
@@ -12,7 +12,11 @@ import types
 import shutil
 debug_slow=0.1
 
-
+def init_tty():
+	try:
+		termwidth=shutil.get_terminal_size()[0]
+	except Exception as E:
+		print(E)
 
 def std_cursorloc():
 	buf = ""
@@ -37,197 +41,233 @@ def std_cursorloc():
 		return None
 	return (int(groups[0]), int(groups[1]))
 
-def stdwrite_org_E(ANSI_cursor):
-	ansi='\033[{n}E'.format(n=ANSI_cursor)
-	def stdout_ansi():
-		sys.stdout.flush()
-		sys.stdout.write(ansi)
-		sys.stdout.flush()
-	return stdout_ansi
-def stdwrite_org_F(ANSI_cursor):
-	ansi='\033[{n}F'.format(n=ANSI_cursor)
-	def stdout_ansi():
-		sys.stdout.flush()
-		sys.stdout.write(ansi)
-		sys.stdout.flush()
-	return stdout_ansi
-def stdwrite_org_G(ANSI_cursor):
-	ansi='\033[{n}G'.format(n=ANSI_cursor)
-	def stdout_ansi():
-		sys.stdout.flush()
-		sys.stdout.write(ansi)
-		sys.stdout.flush()
-	return stdout_ansi
-def stdwrite_org_H(ANSI_cursor):
-	ansi='\033[{n};{m}H'.format(n=ANSI_cursor[0],m=ANSI_cursor[1])
-	def stdout_ansi():
-		sys.stdout.flush()
-		sys.stdout.write(ansi)
-		sys.stdout.flush()
-	return stdout_ansi
-def stdwrite_org_K(ANSI_cursor):
-	ansi='\033[{n}K'.format(n=ANSI_cursor)
-	def stdout_ansi():
-		sys.stdout.flush()
-		sys.stdout.write(ansi)
-		sys.stdout.flush()
-	return stdout_ansi
-def stdwrite_string(text, **k):
-	def dummy(): pass
-	color=k.get('ANSI_Color')
-	fn_pre=k.get('pre') if k.get('pre') else [dummy,]
-	fn_post=k.get('post') if k.get('post') else [dummy,]
+def ANSI_fn(fn,ESC='\033'):
+	def ansi(SEQ):
+		return str_ANSI(ESC=ESC,SEQ=SEQ,FN=fn)
+	return ansi
+	
+def str_ANSI(ESC='\033',SEQ='{SEQ}',FN='{FN}'):
+	return '{ESC}[{SEQ}{FN}'.format(ESC=ESC,SEQ=SEQ,FN=FN)
+	
+ANSI_E= ANSI_fn('E')
+ANSI_F= ANSI_fn('F')
+ANSI_G= ANSI_fn('G')
+ANSI_H= ANSI_fn('H')
+ANSI_K= ANSI_fn('K')
+ANSI_m= ANSI_fn('m')
+
+def ANSI_style(style):
+	STYLES= {
+	'reset'			: 	0,
+	'bold'			: 	1,
+	'ital'			: 	2,
+	'line'			: 	4,
+	'blink'			: 	5,
+	'inv'				: 	7,
+	'strike'		: 	9,
+	'noblink'		:		25,
+	'gray'			:		30,
+	'red'				:		31,
+	'green'			:		32,
+	'yellow'		:		33,
+	'blue'			:		34,
+	'purple'		:		35,
+	'bluegreen'	:		36,
+	'white'			:		37,
+		}
+	return ANSI_m(STYLES.get(style))
+
+def txt_markup(**k):
+	markup=[ANSI_style(style) for style in k.get('m') if k.get('m')]
+	reset=ANSI_style('reset') if k.get('text')[-7:] != ANSI_style('reset') else ''
+	return '{markup}{text}{reset}'.format(markup=str().join(markup),text=k.get('text'),reset=str().join(reset))
+
+def stdout_settxt(*a,**k):
+	text= k.get('txt') if k.get('txt') else str().join(a) if a else ''
+	styles=[style for style in k.get('style') if k.get('style')]
+	txt_styled=txt_markup(text=text,m=styles)
 	def stdout_text():
-		[pre() for pre in fn_pre]
-		sys.stdout.write(text)
-		sys.stdout.flush()
-		[post()for post in fn_post]
+		return str(txt_styled)
 	return stdout_text()
 
-def stdwrite_color(ANSI_color):
-	ansi='\033[{}m'.format(ANSI_color)
-	def stdout_color():
+def setstyle(**k):
+	text='{placeholder}'
+	styles=[style for style in k.get('style') if k.get('style')]
+	txt_styled=txt_markup(text=text,m=styles)
+	def stdout_text(text):
+		return str(txt_styled.format(placeholder=text))
+	return stdout_text
+
+def style(*a,**k):
+ return txt_markup(text=k.get('txt') or a[0],m=k.get('style'))
+	
+def TERM_init(**k):
+	ALLOC=k.get('allocate')
+	COLW=k.get('colw')
+	def init_term():
+		try:
+			termwidth=shutil.get_terminal_size()[0]
+			termheight=shutil.get_terminal_size()[1]
+		except Exception as E:
+			print(E)
+		finally:
+			return termwidth,termheight
+	def update_term():
+			return shutil.get_terminal_size()[0],shutil.get_terminal_size()[1]
+	def allocate(ALLOC):
+		width=init_term()[0]
+		print('\n\n\n')
+		return std_cursorloc()
+	def segment():
+		SEG={}
+		curs_loc=allocate(ALLOC)
+		SEG['top'] = ANSI_H(f'{(curs_loc[0]-ALLOC)};0')
+		SEG['sec'] = ANSI_H(f'{(curs_loc[0]-(ALLOC-1))};0')
+		SEG['bot'] = ANSI_H(f'{(curs_loc[0])};0')
+		SEG['c11'] = ANSI_G(0)
+		SEG['c21'] = ANSI_G(COLW+1)
+		SEG['c12'] = ANSI_G(divmod(COLW,2)[0]+1)
+		SEG['c22'] = ANSI_G(((COLW+1)+divmod(COLW,2)[0]+1))
+		return SEG
+	return segment
+
+def ANSI_org(loc,ALLOC=4,colw=24,termwidth=64):
+	TERM=TERM_init(allocate=ALLOC,colw=colw)
+	ORG= TERM()
+	orgs={
+	'init'				:	f"{ORG['top']}" ,
+	'header'			:	f"{ORG['sec']}" ,
+	'progress'		:	f"{ORG['bot']}" ,
+	
+	'tit1'				:	f"{ORG['top']}{ORG['c11']}" ,
+	'tit1_stat'		:	f"{ORG['top']}{ORG['c12']}" ,
+	'tit2'				:	f"{ORG['top']}{ORG['c21']}" ,
+	'tit2_stat'		:	f"{ORG['top']}{ORG['c22']}" ,
+	'count'				:	f"{ORG['bot']}{ORG['c11']}" ,
+	
+	'proc'				:	f"{ORG['bot']}{ORG['c21']}",
+	'proc_file'		:	f"{ORG['bot']}{ORG['c21']}",
+
+	'clr_left'		:	ANSI_K(1),
+	'clr_right'		:	ANSI_K(2),
+	}
+	return orgs.get(loc)
+
+def stdout_write(*a,**k):
+	def write(*a,**k):
+		sys.stdout.write(ANSI_org(str(k.get('org'))))
+		sys.stdout.write(style(*a,**k))
+		sys.stdout.write(ANSI_style('reset'))
 		sys.stdout.flush()
-		sys.stdout.write(ansi)
-		sys.stdout.flush()
-	return stdout_color
+	return write
+# txt_test=stdout_settxt('test',style=['blue','line'])
+# print(txt_test)                               stdout_write=stdout_write()
 
-def ANSI_cursor(F1=1,L1=32,F2=33,L2=64,SO=12,termwidth=64,curs_init_row=3,curs_init_col=1):
-	org 								= types.SimpleNamespace()
-	org.init						=	stdwrite_org_H([(curs_init_row-3),curs_init_col])
-	org.header					=	stdwrite_org_H([(curs_init_row-2),1])
-	org.progress				=	stdwrite_org_H([(curs_init_row-1),1])
-	# COLS
-	org.title_1					=	stdwrite_org_G(F1)
-	org.title_1_stat		=	stdwrite_org_G(F1+SO)
-	org.title_2					=	stdwrite_org_G(F2)
-	org.title_2_stat		=	stdwrite_org_G(F2+SO)
-	org.title_2_file		=	stdwrite_org_G(F2+6)
-	org.proc						=	stdwrite_org_G(SO)
-	org.count						=	stdwrite_org_G(SO+2)
-	org.alloc						=	stdwrite_org_E(3)		# ROWS:
-
-
-	org.clr_left				=	stdwrite_org_K(1)
-	org.clr_right				=	stdwrite_org_K(2)
-	return org
-
-def ANSI_style():
-	clr=types.SimpleNamespace()
-	clr.reset			=stdwrite_color('0')
-	clr.bold			=stdwrite_color('1')
-	clr.ital			=stdwrite_color('2')
-	clr.underline	=stdwrite_color('4')
-	clr.blink			=stdwrite_color('5')
-	clr.inv				=stdwrite_color('7')
-	clr.strike		=stdwrite_color('9')
-	clr.dunno			=stdwrite_color('9')
-	
-	clr.noblink		=stdwrite_color('25')
-	
-	clr.gray			=stdwrite_color('30')
-	clr.red				=stdwrite_color('31')
-	clr.green			=stdwrite_color('32')
-	clr.yellow		=stdwrite_color('33')
-	clr.blue			=stdwrite_color('34')
-	clr.purple		=stdwrite_color('35')
-	clr.bluegreen	=stdwrite_color('36')
-	clr.white			=stdwrite_color('37')
-	
-	return clr
-
-
-def init_tty():
-	try:
-		termwidth=shutil.get_terminal_size()[0]
-	except Exception as E:
-		print(E)
-
-
-
-
+# print(DONE)
+# style('test3',style=['yellow','blink'])
+def preset():
+	mem= types.SimpleNamespace()
+	style_green=setstyle(style=['green'])
+	style_blue=setstyle(style=['blue'])
+	style_blue=setstyle(style=['red'])
+	style_bold=setstyle(style=['bold'])
+	style_line=setstyle(style=['line'])
+	style_blink=setstyle(style=['blink'])
+	# txt_test\
+	mem.DONE=style_green('DONE')
+	mem.BUSY=style_blink(style_green('BUSY'))
+	mem.CHECK=style_bold('Checking :')
+	mem.PROC=style_bold('Processing :')
+	# stdout_write(txt='test', style=['red','line'], org='tit2')
+	return mem
+stdout_write=stdout_write()
 def cpy(src, dst, force=False) -> None:
 	"""
 	copy progress
 	"""
-	def cli_count(path):
-		org.progress()
-		total = [count(len(d) + len(f)) for p,d,f in os.walk(path,topdown=True)]
-		return total[-1]-1
-	def count(add,tot=[]):
-		global debug_slow
-		tot += [add]
-		stdwrite_string(str(sum(tot)), pre=[org.progress, org.count, clr.red], post=[clr.reset])
-		stdwrite_string(']', pre=[clr.reset], post=[clr.reset])
-		time.sleep(debug_slow)
-		return sum(tot)
-	def cp(srcdir, dest) -> None:
-		"""
-		copys files form srcdir to dest, returns stdout in pipe in realtime
-		"""
-		cmd = shlex.split(f'cp -rvp {srcdir} {dest}')
-		proc_cp = subprocess.Popen(cmd ,stdout=subprocess.PIPE, universal_newlines=True)
-		for line in iter(proc_cp.stdout.readline, ''):
-			yield line.split('->')[0]#'/'.join(line.split('->')[0].split('/')[len(srcdir.split('/')):])
-		proc_cp.stdout.close()
-		return_code = proc_cp.wait()
-		if return_code:
-			raise subprocess.CalledProcessError(return_code, proc_cp)
-	def progress(path, tot, cur):
-		global debug_slow
-		cur= str(cur).zfill(len(str(tot)))
-		ppath=format_path(path)
-		
-		stdwrite_string('Progress: ', pre=[org.progress, clr.bold], post=[clr.reset])
-		stdwrite_string('[')
-		stdwrite_string(cur, pre=[org.proc, clr.red], post=[clr.reset])
-		stdwrite_string('/')
-		stdwrite_string(str(tot), pre=[clr.green], post=[clr.reset])
-		stdwrite_string(']')
-		stdwrite_string('File: ', pre=[org.title_2, clr.bold], post=[clr.reset])
-		stdwrite_string(ppath, pre=[org.title_2_file, clr.yellow], post=[clr.reset])
-		time.sleep(debug_slow)
-		return cur
-	def format_path(path):
-		termwidth=shutil.get_terminal_size()[0]
-		stringwidth=termwidth-38
-		if len(path) > (stringwidth-3):
-			path=f'...{path[-(stringwidth-6):]}'
-			path=path.rjust(stringwidth-6)
-		elif len(path) < ((termwidth-16)/2):
-			path= path.ljust(termwidth-38)
-		return path[:(termwidth-42)]
+	global stdout_write
+	mem=preset()
+
 	print('\n\n')
-	org= ANSI_cursor(curs_init_row=std_cursorloc()[0],curs_init_col=std_cursorloc()[1])
-	clr= ANSI_style()
-	stdwrite_string('PORTAL::' , pre=[org.init,clr.bold, clr.blue], post=[clr.reset])
-	stdwrite_string(f'{src}', pre=[clr.ital, clr.yellow], post=[clr.reset])
-	stdwrite_string(f'\t>->>\t' , pre=[clr.bold, clr.white], post=[clr.reset])
-	stdwrite_string(f'{dst}' , pre=[clr.ital, clr.yellow], post=[clr.reset])
-	stdwrite_string('Checking: ', pre=[org.init, org.header, org.title_1, clr.bold], post=[clr.reset])
-	stdwrite_string('PENDING', pre=[org.title_1_stat])
-	stdwrite_string('Processing: ', pre=[org.title_2, clr.bold], post=[clr.reset])
-	stdwrite_string('PENDING', pre=[org.title_2_stat])
-	stdwrite_string('Progress: ', pre=[org.progress, clr.bold], )
-	stdwrite_string('[', pre=[clr.reset])
-	stdwrite_string('0', pre=[org.proc, clr.reset])
-	stdwrite_string('/')
-	stdwrite_string('0', pre=[org.count])
-	stdwrite_string(']', post=[clr.reset])
-	stdwrite_string('BUSY'.ljust(11, " "), pre=[org.init, org.header, org.title_1_stat, clr.red, clr.blink], post=[clr.noblink, clr.reset])
-	tot=cli_count(path=src)
-	stdwrite_string('Done', pre=[org.header, org.title_1_stat, clr.green], post=[clr.reset])
-	stdwrite_string('Processing: ', pre=[org.title_2, clr.bold], post=[clr.reset])
-	stdwrite_string('BUSY'.ljust(12, " "), pre=[org.title_2_stat, clr.red, clr.blink], post=[clr.noblink, clr.reset])
+
+	stdout_write(txt='PORTAL::' , 								org='init',										style=['bold','blue'], 										)
+	stdout_write(txt='{src}'.format(src=src), 																	style=['ital','yellow'], 									)
+	stdout_write(txt='\t>->>\t' , 																							style=['bold','white'], 										)
+	stdout_write(txt='{dst}'.format(dst=dst) , 																	style=['ital','yellow'],										)
+	stdout_write(txt='Checking: ', 								org='tit1',										style=['bold',],														)
+	stdout_write(txt='PENDING', 									org='tit1_stat',																													)
+	stdout_write(txt='Processing: ', 							org='tit2',										style=['bold'],)
+	stdout_write(txt='PENDING', 									org='tit2_stat'	,																													)
+	stdout_write(txt='Progress: ',								org='progress',								style=['bold'], )
+	stdout_write(txt='[',)
+	stdout_write(txt='0', 												org='proc')
+	stdout_write(txt='/')
+	stdout_write(txt='0', 												org='count',)
+	stdout_write(txt=']',)
+	stdout_write(txt='BUSY'.ljust(11," "), 				org='tit1_stat',							style=['red',' blink'], )
+	tot=cli_count(org, count, path=src)
+	stdout_write('Done', 													org='tit1_stat',							style=['green'],)
+	stdout_write('Processing: ', 									org='tit2',										style=['bold'], )
+	stdout_write('BUSY'.ljust(12, " "), 					org='tit2_stat',							style=[' red',' blink'],)
 	start_timer=timeit.default_timer()
-	cur=[progress(path,tot,idx) for idx,path in enumerate(cp(src, dst))]
+	cur=['progress(org','  path, tot, idx) for idx, path in enumerate(cp(src, dst))']
 	end_timer=timeit.default_timer()
-	stdwrite_string('Done'.ljust(12, " "), pre=[org.header, org.title_2_stat, clr.green], post=[clr.reset])
-	stdwrite_string('Finished: ', pre=[org.progress, org.title_2, clr.bold], post=[clr.reset])
-	stdwrite_string(f'Copied {tot} Files in {end_timer - start_timer} s', pre=[org.title_2_stat,org.clr_right, clr.blue], post=[clr.reset])
-	stdwrite_string('\n\n')
+	stdout_write('Done'.ljust(12, " "), style=['org.header',' tit2_stat, green'], )
+	stdout_write('Finished: ', style=['org.progress',' tit2, bold'], )
+	stdout_write(f'Copied {tot} Files in {end_timer - start_timer} s', style=['tit2_stat','org.right, blue'], )
+	stdout_write('\n\n')
 
+def progress(org, clr, path, tot, cur):
+	global debug_slow
+	cur= str(cur).zfill(len(str(tot)))
+	ppath=format_path(path)
+	
+	stdwrite_string('Progress: ', pre=[org.progress, clr.bold], post=[clr.reset])
+	stdwrite_string('[')
+	stdwrite_string(cur, pre=[org.proc, clr.red], post=[clr.reset])
+	stdwrite_string('/')
+	stdwrite_string(str(tot), pre=[clr.green], post=[clr.reset])
+	stdwrite_string(']')
+	stdwrite_string('File: ', pre=[tit2, clr.bold], post=[clr.reset])
+	stdwrite_string(ppath, pre=[tit2_file, clr.yellow], post=[clr.reset])
+	time.sleep(debug_slow)
+	return cur
 
+def format_path(path):
+	termwidth=shutil.get_terminal_size()[0]
+	stringwidth=termwidth-38
+	if len(path) > (stringwidth-3):
+		path=f'...{path[-(stringwidth-6):]}'
+		path=path.rjust(stringwidth-6)
+	elif len(path) < ((termwidth-16)/2):
+		path= path.ljust(termwidth-38)
+	return path[:(termwidth-42)]
+
+def cp(srcdir, dest) -> None:
+	"""
+	copys files form srcdir to dest, returns stdout in pipe in realtime
+	"""
+	cmd = shlex.split(f'cp -rvp {srcdir} {dest}')
+	proc_cp = subprocess.Popen(cmd ,stdout=subprocess.PIPE, universal_newlines=True)
+	for line in iter(proc_cp.stdout.readline, ''):
+		yield line.split('->')[0]#'/'.join(line.split('->')[0].split('/')[len(srcdir.split('/')):])
+	proc_cp.stdout.close()
+	return_code = proc_cp.wait()
+	if return_code:
+		raise subprocess.CalledProcessError(return_code, proc_cp)
+
+def count(org, clr, add, tot=[]):
+	global debug_slow
+	tot += [add]
+	stdwrite_string(str(sum(tot)), pre=[org.progress, org.count, clr.red], post=[clr.reset])
+	stdwrite_string(']', pre=[clr.reset], post=[clr.reset])
+	time.sleep(debug_slow)
+	return sum(tot)
+
+def cli_count(org, count, path):
+	org.progress()
+	total = [count(len(d) + len(f)) for p,d,f in os.walk(path,topdown=True)]
+	return total[-1]-1
 
 def end(reason='exit'):
 	clr= ANSI_style()
